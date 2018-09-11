1533abcd86bc83060b31a94c73e4c3c7065d6da5
Sam Bramson <sambramson@gmail.com>
2018-09-11 21:29:15 +0100

    added bare bones test driver

---
+++ TestRunner.java
@@ -0,0 +1,10 @@
+public class TestRunner {
+	public static void main(String[] args) {
+		Result r = JUnitCore.runClasses(HelloWorldTest.class);
+		for (Failure f : r.getFailures()) {
+			System.out.println(f.toString());
+		}
+		if (!r.wasSuccessful()) System.out.println("At least one failure, see above.");
+		else System.out.println("All tests passed!");
+	}
+}
